2b9bac7b873c2a8d7f7a24964f4d71536de28033
Axel Cardozo <axelcardozo369@gmail.com>
2026-05-31 21:27:14 -0300

    se implementó en javascript las primeras variables de enlazar con las etiquetas html y se implementó la estructura html para ver cada imagen de cada heroe y el boton de eliminar mediante su id,nombre y imagen y por ultimo se enlazo con etiquetas html del boton y el input

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,3 +5,28 @@ const personajes = [
   { id: 4, nombre: "Abomination", imagen: "https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/images/md/4-abomination.jpg" },
   { id: 5, nombre: "Abraxas", imagen: "https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/images/md/5-abraxas.jpg" }
 ];
+const galeria = document.getElementById("galeria");
+function listaPersonajes(lista){
+  galeria.innerHTML = "";
+  lista.forEach(function({id,nombre,imagen}){
+    const card = `
+    div class="col">
+                <div class="card h-100">
+                    <img src="${imagen}" class="card-img-top" alt="${nombre}">
+                    <div class="card-body text-center">
+                        <h5 class="card-title">${nombre}</h5>
+                        <button class="btn btn-danger btn-sm" onclick="eliminar(${id})">
+                            Eliminar
+                        </button>
+                    </div>
+                </div>
+            </div>
+            `;
+            galeria.innerHTML += card;
+  });
+}
+document.addEventListener("DOMContentLoaded", function(){
+  listaPersonajes(personajes);
+});
+const btnFiltrar = document.getElementById("btnFiltrar");
+const inputFiltro = document.getElementById("inputFiltro");
